29f1d009ede46eb8119544232fade2210baa25fe
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-10-19 20:28:47 +0300

    Добавил элементы на форму

--- Academic performance/MainForm/Form1.Designer.cs
+++ Academic performance/MainForm/Form1.Designer.cs
@@ -28,13 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxGroups = new System.Windows.Forms.ComboBox();
+            this.comboBoxSubjects = new System.Windows.Forms.ComboBox();
+            this.buttonAddDate = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.dataGridViewJournal = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewJournal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonSave);
+            this.panel1.Controls.Add(this.buttonAddDate);
+            this.panel1.Controls.Add(this.comboBoxSubjects);
+            this.panel1.Controls.Add(this.comboBoxGroups);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(510, 86);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Группа";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Дисциплина";
+            // 
+            // comboBoxGroups
+            // 
+            this.comboBoxGroups.FormattingEnabled = true;
+            this.comboBoxGroups.Location = new System.Drawing.Point(70, 11);
+            this.comboBoxGroups.Name = "comboBoxGroups";
+            this.comboBoxGroups.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxGroups.TabIndex = 2;
+            // 
+            // comboBoxSubjects
+            // 
+            this.comboBoxSubjects.FormattingEnabled = true;
+            this.comboBoxSubjects.Location = new System.Drawing.Point(371, 11);
+            this.comboBoxSubjects.Name = "comboBoxSubjects";
+            this.comboBoxSubjects.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSubjects.TabIndex = 3;
+            // 
+            // buttonAddDate
+            // 
+            this.buttonAddDate.Location = new System.Drawing.Point(25, 51);
+            this.buttonAddDate.Name = "buttonAddDate";
+            this.buttonAddDate.Size = new System.Drawing.Size(108, 23);
+            this.buttonAddDate.TabIndex = 4;
+            this.buttonAddDate.Text = "Добавить дату";
+            this.buttonAddDate.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(181, 51);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewJournal
+            // 
+            this.dataGridViewJournal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewJournal.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridViewJournal.Location = new System.Drawing.Point(0, 104);
+            this.dataGridViewJournal.Name = "dataGridViewJournal";
+            this.dataGridViewJournal.Size = new System.Drawing.Size(1013, 557);
+            this.dataGridViewJournal.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1013, 661);
+            this.Controls.Add(this.dataGridViewJournal);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Журнал оценок";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewJournal)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAddDate;
+        private System.Windows.Forms.ComboBox comboBoxSubjects;
+        private System.Windows.Forms.ComboBox comboBoxGroups;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.DataGridView dataGridViewJournal;
     }
 }
 
