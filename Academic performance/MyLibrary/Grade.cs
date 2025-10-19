38ab2fac596964be8479c915827d1855fbc90839
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-10-19 20:36:53 +0300

    Добавил класс оценка

---
+++ Academic performance/MyLibrary/Grade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Grade
+    {
+        public int StudentId { get; set; }
+        public int SubjectId { get; set; }
+        public int GradeValue { get; set; }
+        public DateTime GradeDate { get; set; }
+    }
+}
