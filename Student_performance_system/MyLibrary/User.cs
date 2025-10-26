fea9d3917e310d6bf10f846f8bbbd079eb3757f7
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-10-26 21:27:59 +0300

    Добавил в библиотеку классов класс User

---
+++ Student_performance_system/MyLibrary/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
