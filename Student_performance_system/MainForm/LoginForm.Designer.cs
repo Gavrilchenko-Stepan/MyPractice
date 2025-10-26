35b8f5d444e72ceab66da7e78bc70b43b0c820bd
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-10-26 21:57:32 +0300

    Добавил реализацию формы авторизации

--- Student_performance_system/MainForm/LoginForm.Designer.cs
+++ Student_performance_system/MainForm/LoginForm.Designer.cs
@@ -45,6 +45,7 @@
             this.buttonOk.TabIndex = 0;
             this.buttonOk.Text = "Ок";
             this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
             // 
             // buttonCancel
             // 
@@ -55,6 +56,7 @@
             this.buttonCancel.TabIndex = 1;
             this.buttonCancel.Text = "Отмена";
             this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // label1
             // 
--- Student_performance_system/MainForm/LoginForm.cs
+++ Student_performance_system/MainForm/LoginForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,58 @@ namespace MainForm
 {
     public partial class LoginForm : Form
     {
-        public LoginForm()
+        private readonly AuthService _authService;
+
+        public LoginForm(AuthService authService)
         {
             InitializeComponent();
+            _authService = authService;
         }
 
         private void LoginForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string password = textBoxPassword.Text;
+
+            if (string.IsNullOrEmpty(login))
+            {
+                MessageBox.Show("Введите логин", "Ошибка",
+                              MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxLogin.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Введите пароль", "Ошибка",
+                              MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxPassword.Focus();
+                return;
+            }
+
+            if (_authService.Login(login, password))
+            {
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "Ошибка авторизации",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBoxPassword.Clear();
+                textBoxPassword.Focus();
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
         }
     }
 }
