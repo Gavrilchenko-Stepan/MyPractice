8bc9d5263b6f5132a3a6ec3c9dc5b1620c5f4188
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-10-26 21:25:52 +0300

    Добавил в библиотеку классов класс Teacher

---
+++ Student_performance_system/MyLibrary/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string FullName => $"{LastName} {FirstName} {MiddleName}";
+    }
+}
