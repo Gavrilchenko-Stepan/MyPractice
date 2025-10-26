2d2871f817f28deb0bea5787da56d9f29e8459da
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-10-26 22:01:49 +0300

    Добавил вывод формы авторизации на главную форму

--- Student_performance_system/MainForm/MainForm.cs
+++ Student_performance_system/MainForm/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,31 @@ namespace MainForm
 {
     public partial class MainForm : Form
     {
+        private AuthService _authService;
+
         public MainForm()
         {
             InitializeComponent();
+            InitializeServices();
+            ShowLoginForm();
+        }
+
+        private void InitializeServices()
+        {
+            IUserRepository userRepository = new UserRepository();
+            _authService = new AuthService(userRepository);
+        }
+
+        private void ShowLoginForm()
+        {
+            using (var loginForm = new LoginForm(_authService))
+            {
+                if (loginForm.ShowDialog() != DialogResult.OK)
+                {
+                    Application.Exit();
+                    return;
+                }
+            }
         }
     }
 }
