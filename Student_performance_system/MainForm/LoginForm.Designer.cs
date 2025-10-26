5bdcb88a45c788ce998811a27cd87e539230d685
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-10-26 22:17:57 +0300

    ошибка с закрытием формы авторизации при вводе неверных данных исправлена

--- Student_performance_system/MainForm/LoginForm.Designer.cs
+++ Student_performance_system/MainForm/LoginForm.Designer.cs
@@ -38,7 +38,6 @@
             // 
             // buttonOk
             // 
-            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.buttonOk.Location = new System.Drawing.Point(213, 195);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(75, 23);
