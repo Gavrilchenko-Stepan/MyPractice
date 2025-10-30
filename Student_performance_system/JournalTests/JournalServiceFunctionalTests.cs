0cbac8ef4c03c3923d480dbc2683535643e97088
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-10-30 18:58:36 +0300

    Добавил тест на загрузку журнала для несуществующего предмета

--- Student_performance_system/JournalTests/JournalServiceFunctionalTests.cs
+++ Student_performance_system/JournalTests/JournalServiceFunctionalTests.cs
@@ -55,5 +55,30 @@ namespace JournalTests
                 Assert.IsFalse(result.Grades.ContainsKey((expected.StudentId, expected.Date)));
             }
         }
+
+        //Тест 3: Загрузка журнала для несуществующего предмета
+        [TestMethod]
+        public void GetJournalData_WithNonExistentSubject_ReturnsStudentsWithoutGrades()
+        {
+            // Arrange
+            string groupName = "П-20";
+            string subjectName = "Несуществующий предмет";
+
+            var expectedGrades = new List<(int StudentId, DateTime Date, int? Grade)>();
+
+            JournalData result = _journalService.GetJournalData(groupName, subjectName);
+
+            Assert.AreEqual("П-20", result.GroupName);
+            Assert.AreEqual("Несуществующий предмет", result.SubjectName);
+            Assert.AreEqual(2, result.Students.Count);
+            Assert.AreEqual(0, result.LessonDates.Count);
+            Assert.AreEqual(0, result.Grades.Count);
+            Assert.AreEqual("Кузнецов Алексей Викторович", result.Students[0].FullName);
+
+            foreach (var expected in expectedGrades)
+            {
+                Assert.IsFalse(result.Grades.ContainsKey((expected.StudentId, expected.Date)));
+            }
+        }
     }
 }
