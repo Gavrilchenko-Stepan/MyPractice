7037828b197cde97646ba2c175220eba669274c6
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-10-31 22:01:38 +0300

    Добавил класс Row, который представляет строку в талице журнала студент-дата-оценка

---
+++ Student_performance_system/MyLibrary/Row.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Row
+    {
+        public Student Student { get; set; }
+        public List<(DateTime Date, int? Grade)> Grades { get; set; } = new List<(DateTime, int?)>();
+    }
+}
---
+++ Student_performance_system/Tests/TJournalService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class TJournalService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
