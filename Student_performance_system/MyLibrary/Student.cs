d696e3269acdfe3928aea84073eb05c5a757c123
Gavrilchenko-Stepan <atom47228@gmail.com>
2025-11-01 12:53:04 +0300

    Добавил класс Student

---
+++ Student_performance_system/MyLibrary/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Student
+    {
+        int StudentId { get; set; }
+        string FullName { get; set; }
+        string GroupName { get; set; }
+    }
+}
