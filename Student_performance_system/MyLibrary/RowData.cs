7f7724a1b773ec5df2aefd01ae8147273323988f
Gavrilchenko-Stepan <atom47228@gmail.com>
2025-11-01 12:48:31 +0300

    Внёс изменения в класс Row (RowData)

--- Student_performance_system/MyLibrary/RowData.cs
+++ Student_performance_system/MyLibrary/RowData.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyLibrary
 {
-    public class Row
+    public class RowData
     {
         public Student Student { get; set; }
-        public List<(DateTime Date, int? Grade)> Grades { get; set; } = new List<(DateTime, int?)>();
+        public List<Grade> Grades { get; set; }
     }
 }
