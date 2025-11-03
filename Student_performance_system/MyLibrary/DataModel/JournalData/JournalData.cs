c6b80e5326f23283144b0b02d4599f1e632fd554
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-11-03 20:18:36 +0300

    Добавил класс Данные журнала JournalData и немного сделал удобнее навигацию, создав папки для удобства Класс которые перераспределил по папкам был добавлены с тестами

---
+++ Student_performance_system/MyLibrary/DataModel/JournalData/JournalData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.DataModel.JournalData
+{
+    public class JournalData
+    {
+        public string GroupName { get; set; }
+        public string SubjectName { get; set; }
+        public List<RowData> Rows { get; set; } = new List<RowData>();
+    }
+}
