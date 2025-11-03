396276847d3cbcb35e89939a038ed1a8bff33b51
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-11-03 21:34:56 +0300

    Добавил представление журнала

---
+++ Student_performance_system/MyLibrary/View/IJournalView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.View
+{
+    public interface IJournalView
+    {
+        string GroupName { get; }
+        string SubjectName { get; }
+    }
+}
