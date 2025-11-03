4b72923178f52f949fdcb60d718023a6c5da5ea3
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-11-03 21:36:27 +0300

    Добавил класс презентер для журнала

---
+++ Student_performance_system/MyLibrary/Presenter/JournalPresenter.cs
@@ -0,0 +1,21 @@
+﻿using MyLibrary.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Presenter
+{
+    public class JournalPresenter
+    {
+        private readonly IJournalView _view;
+        private readonly JournalService _journalService;
+
+        public JournalPresenter(IJournalView view, JournalService journalService)
+        {
+            _view = view;
+            _journalService = journalService;
+        }
+    }
+}
