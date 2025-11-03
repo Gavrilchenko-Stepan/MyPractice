0209d1aa16bd60f8f6a2d41cd87ebb5bb2f34456
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-11-03 20:52:45 +0300

    Добавил класс для работы с базой данных через конфигурацию. Также настроил app.config

---
+++ Student_performance_system/MyLibrary/AppConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class AppConfig
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"];
+                if (connectionString == null)
+                    throw new Exception("Не найдена строка подключения 'MySqlConnection' в App.config");
+
+                return connectionString.ConnectionString;
+            }
+        }
+
+        public static string DefaultGroup => "П-10";
+        public static string DefaultSubject => "Математика";
+    }
+}
