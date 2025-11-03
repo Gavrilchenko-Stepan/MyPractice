ec035aa3ef18bef557d3772f5fc3f6302904bb82
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-11-03 21:06:22 +0300

    Создал класс MySqlGradeRepository для работы с бд и добавил метод который возвращает список оценок

---
+++ Student_performance_system/MyLibrary/Model/Grade/MySqlGradeRepository.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Repositories
+{
+    public class MySqlGradeRepository : IGradeRepository
+    {
+        private readonly string _connectionString;
+
+        public MySqlGradeRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Grade> GetGradesByGroupAndSubject(string groupName, string subjectName)
+        {
+            var grades = new List<Grade>();
+
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var sql = @"
+                SELECT 
+                    g.student_id, 
+                    g.grade_date as lesson_date, 
+                    CAST(g.grade_value as SIGNED) as grade_value
+                FROM Grades g 
+                INNER JOIN Students s ON g.student_id = s.student_id 
+                INNER JOIN Subjects sub ON g.subject_id = sub.subject_id 
+                WHERE s.group_name = @GroupName AND sub.subject_name = @SubjectName
+                ORDER BY g.grade_date";
+
+                using (var command = new MySqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@GroupName", groupName);
+                    command.Parameters.AddWithValue("@SubjectName", subjectName);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            grades.Add(new Grade
+                            {
+                                StudentId = reader.GetInt32("student_id"),
+                                LessonDate = reader.GetDateTime("lesson_date"),
+                                GradeValue = reader.GetInt32("grade_value")
+                            });
+                        }
+                    }
+                }
+            }
+
+            return grades;
+        }
+
+        public List<DateTime> GetLessonDates(string groupName, string subjectName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
