912ada182e56c596e67ce0f261d8359ec099a8e1
Gavrilchenko-Stepan <atom47228@gmail.com>
2025-11-18 08:32:32 +0300

    Добавил поля, конструктор в класс LessonDate. ToString() переопределяется для удобного строкового представления объекта LessonData.

--- Student_performance_system/MyLibrary/DataModel/LessonData.cs
+++ Student_performance_system/MyLibrary/DataModel/LessonData.cs
@@ -8,5 +8,20 @@ namespace MyLibrary.DataModel
 {
     public class LessonData
     {
+        public DateTime Date { get; set; }
+        public int? LessonNumber { get; set; }
+
+        public LessonData(DateTime date, int? lessonNumber = null)
+        {
+            Date = date;
+            LessonNumber = lessonNumber;
+        }
+
+        public override string ToString()
+        {
+            return LessonNumber.HasValue
+                ? $"{Date:dd.MM.yyyy} (пара {LessonNumber.Value})"
+                : $"{Date:dd.MM.yyyy}";
+        }
     }
 }
