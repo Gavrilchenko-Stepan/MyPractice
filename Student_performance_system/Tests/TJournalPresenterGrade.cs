38bc5ec94e22e14553f0e2842ea066ebb9a968ba
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-11-26 22:12:01 +0300

    Добавил тест на проверку взаимодействия между UI и бизнес-логикой: успешное сохранени и ошибка сохранения

---
+++ Student_performance_system/Tests/TJournalPresenterGrade.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MyLibrary;
+using MyLibrary.Presenter;
+using MyLibrary.Repositories;
+using MyLibrary.View;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class TJournalPresenterGrade
+    {
+        [DataTestMethod]
+        [DataRow(true, "Успешное сохранение")]
+        [DataRow(false, "Ошибка сохранения")]
+        public void UpdateGrade_DifferentResults_ShouldHandleCorrectly(bool serviceResult, string description)
+        {
+            var mockView = new Mock<IJournalView>();
+            var mockJournalService = new Mock<JournalService>(
+                Mock.Of<IStudentRepository>(),
+                Mock.Of<IGradeRepository>(),
+                Mock.Of<IJournalCommandRepository>());
+
+            mockView.Setup(v => v.SubjectName).Returns("Математика");
+            mockJournalService.Setup(s =>
+                s.UpdateGrade(1, "Математика", It.IsAny<DateTime>(), 1, 5)).Returns(serviceResult);
+
+            var loadJournalCalled = false;
+            mockView.Setup(v => v.LoadJournal()).Callback(() => loadJournalCalled = true);
+
+            var presenter = new JournalPresenter(mockView.Object, mockJournalService.Object);
+
+            presenter.UpdateGrade(1, DateTime.Now, 1, 5);
+
+            if (serviceResult)
+            {
+                mockView.Verify(v => v.ShowSuccessMessage("Оценка успешно сохранена"), Times.Once, description);
+                Assert.IsTrue(loadJournalCalled, $"Журнал должен быть обновлен: {description}");
+            }
+            else
+            {
+                mockView.Verify(v => v.ShowErrorMessage("Не удалось сохранить оценку"), Times.Once, description);
+            }
+        }
+    }
+}
