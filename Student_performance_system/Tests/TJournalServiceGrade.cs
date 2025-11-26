818fd4d91ca3504c35745159a92548b7ee896fca
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-11-26 22:05:12 +0300

    Добавил тест на проверку не валидных оценко при сохранении

--- Student_performance_system/Tests/TJournalServiceGrade.cs
+++ Student_performance_system/Tests/TJournalServiceGrade.cs
@@ -37,5 +37,27 @@ namespace Tests
             mockJournalCommandRepository.Verify(r =>
                 r.UpdateGrade(1, 1, It.IsAny<DateTime>(), 1, gradeValue), Times.Once, description);
         }
+
+        [DataTestMethod]
+        [DataRow(1, "Некорректная оценка 1")]
+        [DataRow(6, "Некорректная оценка 6")]
+        [DataRow(0, "Некорректная оценка 0")]
+        public void UpdateGrade_InvalidGrades_ShouldThrowException(int invalidGrade, string description)
+        {
+            var mockStudentRepository = new Mock<IStudentRepository>();
+            var mockGradeRepository = new Mock<IGradeRepository>();
+            var mockJournalCommandRepository = new Mock<IJournalCommandRepository>();
+
+            mockGradeRepository.Setup(r => r.GetSubjectId("Математика")).Returns(1);
+
+            var journalService = new JournalService(
+                mockStudentRepository.Object,
+                mockGradeRepository.Object,
+                mockJournalCommandRepository.Object);
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                journalService.UpdateGrade(1, "Математика", DateTime.Now, 1, invalidGrade),
+                description);
+        }
     }
 }
