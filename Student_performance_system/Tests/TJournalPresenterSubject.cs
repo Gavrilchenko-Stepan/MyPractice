6c1dd640a3915c0bf223f995448b79ada0573a49
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-11-27 21:04:25 +0300

    Тест на проверку корректного делегирования вызова репозиторию

---
+++ Student_performance_system/Tests/TJournalPresenterSubject.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class TJournalPresenterSubject
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Student_performance_system/Tests/TJournalServiceSubject.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MyLibrary;
+using MyLibrary.Repositories;
+using System;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class TJournalServiceSubject
+    {
+        [TestMethod]
+        [DataRow(new string[] { "Математика", "Физика" }, DisplayName = "Два предмета")]
+        [DataRow(new string[] { "Математика" }, DisplayName = "Один предмет")]
+        public void GetSubjects_WithMockRepository_ReturnsSameData(string[] mockSubjects)
+        {
+            // Arrange
+            var mockRepository = new Mock<IGradeRepository>();
+            var mockStudentRepo = new Mock<IStudentRepository>();
+            var mockCommandRepo = new Mock<IJournalCommandRepository>();
+
+            mockRepository.Setup(r => r.GetSubjects()).Returns(mockSubjects.ToList());
+            var service = new JournalService(mockStudentRepo.Object, mockRepository.Object, mockCommandRepo.Object);
+
+            // Act
+            var result = service.GetSubjects();
+
+            // Assert
+            CollectionAssert.AreEqual(mockSubjects, result.ToArray());
+            mockRepository.Verify(r => r.GetSubjects(), Times.Once);
+        }
+    }
+}
